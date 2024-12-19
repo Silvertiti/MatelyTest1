8029df9918166b247fbe30eb7e5cc2307483a3d0
Silvertiti <metheotim2@gmail.com>
2024-12-19 15:24:14 +0100

    Mise en place Texte Input 2éme page

--- screens/SecondScreen.js
+++ screens/SecondScreen.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 const DetailsScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>2 eme ecran</Text>
-    </View>
-  );
+    const [text, setText] = useState(''); 
+
+    return (
+        <View style={styles.container}>
+        <Text style={styles.text}>Nouveau Poste</Text>
+        <TextInput
+        style={styles.input}
+        placeholder='Entrez votre texte'
+        value={text}
+        onChangeText={(newText) => setText(newText)}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 20,
+    },
+    text: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    input: {
+        height: 30,
+        width: 200,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 20,
+    },
 });
 
 export default DetailsScreen;
