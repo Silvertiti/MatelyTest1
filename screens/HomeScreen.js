244aa14db2c66f8af82f585ab93c01ba0a7e06e5
Silvertiti <metheotim2@gmail.com>
2024-12-19 15:01:30 +0100

    Mise en place fenetre 2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'Top',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
